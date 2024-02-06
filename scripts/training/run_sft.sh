a5fa99632e3c7674901b123bc8b84173600699a0
tangyu <super_tangyu@foxmail.com>
2024-02-06 18:13:18 +0800

    更新文件

--- scripts/training/run_sft.sh
+++ scripts/training/run_sft.sh
@@ -15,13 +15,13 @@ dataset_dir=/home/tangyu/huggingface_download/ft_data
 per_device_train_batch_size=1
 per_device_eval_batch_size=1
 gradient_accumulation_steps=8
-max_seq_length=2048
-output_dir=/home/tangyu/huggingface_download/ft_202401221751
-validation_file=/home/tangyu/huggingface_download/ft_data/validate_ft20240119.json
+max_seq_length=1024
+output_dir=/home/tangyu/huggingface_download/ft_gx_20240202020030
+validation_file=/home/tangyu/huggingface_download/ft_data/validate_gx.json
 
 deepspeed_config_file=ds_zero2_no_offload.json
 
-torchrun --nnodes 1 --nproc_per_node 4 run_clm_sft_with_peft.py \
+torchrun --nnodes 1 --nproc_per_node 2 run_clm_sft_with_peft.py \
     --deepspeed ${deepspeed_config_file} \
     --model_name_or_path ${pretrained_model} \
     --tokenizer_name_or_path ${chinese_tokenizer_path} \
@@ -32,7 +32,7 @@ torchrun --nnodes 1 --nproc_per_node 4 run_clm_sft_with_peft.py \
     --do_eval \
     --seed $RANDOM \
     --fp16 \
-    --num_train_epochs 1 \
+    --num_train_epochs 15 \
     --lr_scheduler_type cosine \
     --learning_rate ${lr} \
     --warmup_ratio 0.03 \
@@ -58,7 +58,7 @@ torchrun --nnodes 1 --nproc_per_node 4 run_clm_sft_with_peft.py \
     --modules_to_save ${modules_to_save} \
     --torch_dtype float16 \
     --validation_file ${validation_file} \
-    --load_in_kbits 8 \
+    --load_in_kbits 16 \
     --save_safetensors False \
     --gradient_checkpointing \
     --ddp_find_unused_parameters False
